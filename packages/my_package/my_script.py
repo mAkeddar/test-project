75ffe35bbec83694c6b2fb6fa81c52ff60533833
Guillaume <chacungu@gmail.com>
2024-10-04 12:23:38 +0200

    Beginner - The DTProject

--- packages/my_package/my_script.py
+++ packages/my_package/my_script.py
@@ -1,2 +1,10 @@
 message = "\nHello World!\n"
 print(message)
+
+import numpy as np
+print(np.array([1,2,3,4]).mean())
+
+import os
+vehicle_name = os.environ['VEHICLE_NAME']
+message = f"\nHello from {vehicle_name}!\n"
+print(message)
